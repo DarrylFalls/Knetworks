f416d6f6bab527f5d5daa7d7af64007f003cc3ea
Darryl Falls <darryl.a.falls@gmial.com>
2021-12-13 13:01:15 -0500

    setting up routes

--- client/src/App.js
+++ client/src/App.js
@@ -9,20 +9,22 @@ import Categories from './screens/Categories/Categories';
 import CategoryQuestions from './screens/CategoryQuestions/CategoryQuestions';
 import QuestionDetail from './screens/QuestionDetail/QuestionDetail';
 import CreateQuestion from './screens/CreateQuestion/CreateQuestion';
+import {useState, useEffect} from 'react'
 
 function App() {
+  const [loggedIn, setLoggedIn] = useState(false)
   return (
     <div className="App">
       <Layout>
         <Routes>
-          <Route path='/' exact element={<Home />} />
+          <Route path='/' exact element={<Home loggedIn={loggedIn}/>} />
           <Route path='/login' element={<Login />} />
           <Route path='/sign-up' element={<SignUp />} />
           <Route path='/welcome' element={<Welcome />} />
           <Route path='/categories' element={<Categories />} />
-          <Route path='/categories/:id' element={<CategoryQuestions />} />
-          <Route path='/question/:id' element={<QuestionDetail />} />
-          <Route path='/post_question' element={<CreateQuestion />} />
+          <Route path='/categories/:id' element={<CategoryQuestions loggedIn={loggedIn}/>} />
+          <Route path='/question/:id' element={<QuestionDetail loggedIn={loggedIn}/>} />
+          <Route path='/post_question' element={<CreateQuestion loggedIn={loggedIn}/>} />
         </Routes>
       </Layout>
     </div>
