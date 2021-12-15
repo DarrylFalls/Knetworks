4b69163de98614999ed9f77393aff236c8579d1d
Darryl Falls <darryl.a.falls@gmial.com>
2021-12-15 13:36:37 -0500

    styling

--- client/src/screens/Categories/Categories.jsx
+++ client/src/screens/Categories/Categories.jsx
@@ -1,17 +1,19 @@
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
+import './Categories.css'
 
 const Categories = ({categories}) => {
   return (
-    <div>
-      <div>Categories</div>
-      {
-        categories.map((category) => (
-        <div>
-          <Link to={`/categories/${category.id}`}>
-            {category.name}
-          </Link>
-        </div>
-      ))}
+    <div className='main-category-div'>
+      <div className='category-title-div'>Categories</div>
+      <div className='category-link-container-div'>
+        {categories.map((category) => (
+          <div className='category-link-div'>
+            <Link to={`/categories/${category.id}`} className='category-link'>
+              {category.name}
+            </Link>
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
--- client/src/screens/Home/Home.jsx
+++ client/src/screens/Home/Home.jsx
@@ -1,10 +1,18 @@
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
+import './Home.css'
 
 const Home = () => {
   return (
     <div>
-      <div>home page</div>
-      <Link to='/categories'>Pick a category</Link>
+      <div className='home-text-div-1'>Bringing people together through the power of learning and teamwork, in order to make the world a better and smarter place.</div>
+      <div className='home-text-div-2'>
+        <div className='home-sub-text-div-1'>Knetworks is a place to ask questions and bring ideas together.</div>
+        <div className='home-sub-text-div-2'>Need help understanding a concept, or just curious? Create an account, and ask a question to get help from other users.</div>
+      </div>
+      
+        <Link to='/categories' className='home-category-link'><div className='home-category-link-div'>Pick a category to see questions that have already been posted</div></Link>
+      
+      
     </div>
   )
 }
